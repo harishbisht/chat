bf66343fb6a0195f7991dbd5261823a97127d59d
harish <harishbisht092@gmail.com>
2016-02-14 20:16:03 +0530

    master

--- chat/settings.py
+++ chat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rqq_r^=6v7k94-v9*@m!f*^o=b+s9fhg6(4e3v!hgnl)a^)sn6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
